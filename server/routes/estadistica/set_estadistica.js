5726252f1b8ef2636872745a759d56734929abea
Jorge Lagos <jrlagosc@unah.hn>
2022-07-22 09:38:02 -0600

    GetAll Estadisticas

---
+++ server/routes/estadistica/set_estadistica.js
@@ -0,0 +1,78 @@
+// requires scripts assistant
+const {contains_data, traerdatos} = require('../../scripts/scripts')
+
+// requires conexion db
+const Conexion = require('../../config/dbconnect')
+
+// requires express and more
+const router = require('express').Router()
+
+
+//Post Estadistica
+router.post('/ins_estadistica', (req,res)=>{
+    const {titulo_estadistica,desc_estadistica,url_archivo,formato_estadistica 
+        ,correo_electronico,hora_generar_estadistica,tipo_estadistica,ind_estadistica
+        ,tiempo_generar_estadistica,cod_usuario} = req.body;
+        const sql = "CALL Ins_Estadisticas(?,?,?,?,?,?,?,?,?,?)"
+
+        if(!contains_data(nom_pais)){
+            return res.status(400).json({
+                ok: false,
+                err:{
+                    message: 'No Valido.'
+                }
+            })
+        }
+
+        Conexion.query(titulo_estadistica,desc_estadistica,url_archivo,formato_estadistica 
+            ,correo_electronico,hora_generar_estadistica,tipo_estadistica,ind_estadistica
+            ,tiempo_generar_estadistica,cod_usuario , (err, result) =>{
+            if(err) throw err
+    
+            res.json({
+                ok: true,
+                result: result[0]
+            })
+        })
+})
+
+// Get All Estadistica
+router.get('/estadistica', (req, res) =>{
+  const sql = 'CALL GetAll_Estadisticas()'
+
+    Conexion.query(sql, (err, result) =>{
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+        res.json({
+            ok: true,
+            result: result[0]
+        })
+    })
+
+})
+
+
+//UdDate Estadistica
+router.put('/upt_estadistica:cod_estadistica',(req, res)=>{
+    const {titulo_estadistica,desc_estadistica,url_archivo,formato_estadistica 
+        ,correo_electronico,hora_generar_estadistica,tipo_estadistica,ind_estadistica
+        ,tiempo_generar_estadistica,cod_usuario} = req.body;
+        const {cod_estadistica} = req.params
+        const sql = 'CALL Upt_Estadisticas(?,?,?,?,?,?,?,?,?,?)'
+
+        Conexion.query(titulo_estadistica,desc_estadistica,url_archivo,formato_estadistica 
+            ,correo_electronico,hora_generar_estadistica,tipo_estadistica,ind_estadistica
+            ,tiempo_generar_estadistica,cod_usuario , (err, result) =>{
+            if(err) throw err
+    
+            res.json({
+                ok: true,
+                result: result[0]
+            })
+        })
+})
+module.exports = router;
--- server/routes/main.js
+++ server/routes/main.js
@@ -5,7 +5,8 @@ const app = express()
 // paises
 app.use(require('./direcciones/set_pais'))
 
-
+// estadistica
+app.use(require('./estadistica/set_estadistica'))
 
 module.exports = app;
 
