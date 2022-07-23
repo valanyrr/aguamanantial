c565222ba77e0a4bf6bc437392d092cc51227abe
JoseQuiroz <jose.acuna@unah.hn>
2022-07-23 16:01:39 -0600

    Insert de datos

--- server/routes/ventasycompras/Ventas.js
+++ server/routes/ventasycompras/Ventas.js
@@ -3,3 +3,27 @@ const Conexion = require('../../config/dbconnect')
 
 // requires express and more
 const router = require('express').Router()
+
+//insert Venta
+router.post('/Ventas', (req, res) =>{
+    const {cant_Ventas} = req.body;
+    const sql = "CALL Cant_Ventas(?)"
+
+    if(!contains_data(Cant_Ventas)){
+        return res.status(500).json({
+            ok: false,
+            err:{
+                message: 'cantidad no valido.'
+            }
+        })
+    }
+   
+    Conexion.query(sql, cant_Ventas, (err, result) =>{
+        if(err) throw error
+
+        res.json({
+            ok: true,
+            result
+        })
+    })
+})
