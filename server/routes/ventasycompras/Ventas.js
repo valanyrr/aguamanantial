645eb8abc503296f5f5b6977b988444fa614541b
JoseQuiroz <jose.acuna@unah.hn>
2022-07-23 17:34:52 -0600

    trabajando

--- server/routes/ventasycompras/Ventas.js
+++ server/routes/ventasycompras/Ventas.js
@@ -5,76 +5,130 @@ const Conexion = require('../../config/dbconnect')
 const router = require('express').Router()
 
 //insert Venta
-router.post('/Ventasycompras', (req, res) =>{
-    const {cant_Ventas} = req.body;
-    const sql = "CALL Cant_Ventas(?)"
+for( i=0; i < MiItems.length; i++){
+    Valores += '<tr>'+
+        '<td>'+ MiItems[i].ID +'</td>'+
+        '<td>'+ MiItems[i].ID_SOCIO +'</td>'+
+        '<td>'+ MiItems[i].FECHA_PEDIDO +'</td>'+
+        '<td>'+ MiItems[i].DETALLE +'</td>'+
+        '<td>'+ MiItems[i].SUB_TOTAL +'</td>'+
+        '<td>'+ MiItems[i].TOTAL_ISV +'</td>'+
+        '<td>'+ MiItems[i].TOTAL +'</td>'+
+        '<td>'+ MiItems[i].FECHA_ENTREGA +'</td>'+
+        '<td>'+ MiItems[i].ESTADO +'</td>'+
+        '<td>'+ 
+        '<button class="btn btn-outline-secondary" onclick="CargarPedidoID('+ MiItems[i].ID +')">Editar</button>'+ 
+        '<button class="btn btn-outline-danger" onclick="EliminarPedido('+ MiItems[i].ID +')">Eliminar</button>'
+        '</td>'+ 
+    '</tr>'; 
+    $('.Pedidos').html(Valores);
+}
 
-    if(!contains_data(Cant_Ventas)){
-        return res.status(500).json({
-            ok: false,
-            err:{
-                message: 'cantidad no valido.'
-            }
-        })
-    }
-   
-    Conexion.query(sql, cant_Ventas, (err, result) =>{
-        if(err) throw error
+function AgregarPedido(){
+var datospedidos={
+ID_SOCIO:$('#ID_SOCIO').val(),
+FECHA_PEDIDO:$('#FECHA_PEDIDO').val(),
+DETALLE:$('#DETALLE').val(),
+SUB_TOTAL:$('#SUB_TOTAL').val(),
+TOTAL_ISV:$('#TOTAL_ISV').val(),
+TOTAL:$('#TOTAL').val(),
+FECHA_ENTREGA:$('#FECHA_ENTREGA').val(),
+ESTADO:$('#ESTADO').val()
+};
+var datospedidosjson = JSON.stringify(datospedidos);
 
-        res.json({
-            ok: true,
-            result
-        })
-    })
-})
+$.ajax({
+url: UrlPostPedidos,
+type: 'POST',
+data: datospedidosjson,
+dataType: 'JSON',
+contenttype: 'application/json',
+success: function(reponse){
+console.log(reponse)
+},
+error: function(){
+alert('Error al agregar nuevo pedido')
+}        
+});
+alert('Pedido agregado exitosamente');
+}
 
-// Get ventas
-router.get('/ventas', (req, res) =>{
-    const {cod_depto} = req.params
-    const sql = 'CALL Get_Ventas()'
+function CargarPedidoID(idpedido){ 
+var datospedidos = { 
+ID: idpedido 
+} 
+var datospedidosjson = JSON.stringify(datospedidos); 
 
-    Conexion.query(sql, (err, result) =>{
-        if(err){
-            return res.status(500).json({
-                ok: false,
-                err
-            })
-        }
+$.ajax({ 
+url: UrlGetUno, 
+type: 'POST', 
+data: datospedidosjson, 
+datatype: 'application/json', 
+success:function(response)
+{ 
+var MiItems = response 
+$('#ID_SOCIO').val(MiItems[0].ID_SOCIO); 
+$('#FECHA_PEDIDO').val(MiItems[0].FECHA_PEDIDO); 
+$('#DETALLE').val(MiItems[0].DETALLE); 
+$('#SUB_TOTAL').val(MiItems[0].SUB_TOTAL); 
+$('#TOTAL_ISV').val(MiItems[0].TOTAL_ISV); 
+$('#TOTAL').val(MiItems[0].TOTAL); 
+$('#FECHA_ENTREGA').val(MiItems[0].FECHA_ENTREGA); 
+$('#ESTADO').val(MiItems[0].ESTADO); 
 
-        res.json({
-            ok: true,
-            result: result[0]
-        })
-    })
+var btnactualizar= '<input type="submit" id="btnactualizar" onclick="ActualizarPedido('+ MiItems[0].ID +')"' + 
+'value="Actualizar pedido" class="btn btn-primary"></input>'; 
+$('.btnpedido').html(btnactualizar); 
+} 
+});
+}
 
-})
+function ActualizarPedido(idpedido){
+var datospedidos = {
+ID: idpedido,
+ID_SOCIO:$('#ID_SOCIO').val(),
+FECHA_PEDIDO:$('#FECHA_PEDIDO').val(),
+DETALLE:$('#DETALLE').val(),
+SUB_TOTAL:$('#SUB_TOTAL').val(),
+TOTAL_ISV:$('#TOTAL_ISV').val(),
+TOTAL:$('#TOTAL').val(),
+FECHA_ENTREGA:$('#FECHA_ENTREGA').val(),
+ESTADO:$('#ESTADO').val(),
+};
+var datospedidosjson = JSON.stringify(datospedidos);
 
-// UpDate ventas
-router.post('/ventasycompras', (req, res) =>{
-    const {nom_depto} = req.body;
-    const {cod_depto} = req.params
-    const sql = "CALL Cant_Ventas(?)"
-
-    if(!contains_data(nom_depto)){
-        return res.status(2000).json({
-            ok: false,
-            err:{
-                message: 'cantidades no valido.'
-            }
-        })
-    }
-   
-    Conexion.query(sql, nom_depto, (err, result) =>{
-        if(err) throw err
-
-        res.json({
-            ok: true,
-            result
-        })
-    })
-})
+$.ajax({
+url: UrlPutPedidos,
+type: 'PUT',
+data: datospedidosjson,
+datatype: 'JSON',
+contentType: 'application/json',
+success: function(response){
+console.log(response)
+}
+});
+alert("Pedido Actualizado Exitosamente");
+}
 
 
+function EliminarPedido(idpedido){
+var datospedidos = {
+ID: idpedido
+};
+var datospedidosjson = JSON.stringify(datospedidos);
+
+$.ajax({
+url: UrlDelPedidos,
+type: 'DELETE',
+data: datospedidosjson,
+dataType: 'JSON',
+contentType: 'application/json',
+success: function(reponse){
+console.log(reponse);
+}
+});
+alert("Pedido Eliminado Exitosamente");
+}
 
 
 
